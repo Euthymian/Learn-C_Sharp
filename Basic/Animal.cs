e1d6f5d0619387a2fccf8afed47e068986709319
GoldenGoat <thanhstryshard@gmail.com>
2024-04-24 19:19:41 -0700

    Snake

---
+++ Basic/Animal.cs
@@ -0,0 +1,43 @@
+public abstract class Animal
+ {
+    protected string Weight { get; set; }
+    protected string Height { get; set; }
+
+    public Animal(string weight, string height)
+    {
+        this.Weight = weight;
+        this.Height = height;
+    }
+    public abstract void PrintInfo();
+}
+
+class Cat : Animal
+{
+    protected string Name { get; set; }
+    public Cat(string weight, string height, string name)
+    : base(weight, height)
+    {
+        this.Name = name;
+    }
+
+    public override void PrintInfo()
+    {
+        System.Console.WriteLine("Cat: ");
+        Console.WriteLine("Weight : {0}" + '\n' + "Height: {1}" + '\n' + "Name: {2}", this.Weight, this.Height, this.Name);
+    }
+}
+
+class Kitten : Cat
+{
+    private int age;
+    public Kitten(string weight, string height, string name, int age)
+    : base(weight, height, name)
+    {
+        this.age = age;
+    }
+    public override void PrintInfo()
+    {
+        System.Console.WriteLine("Kitten: ");
+        Console.WriteLine("Weight : {0}" + '\n' + "Height: {1}" + '\n' + "Name: {2}" + '\n' + "Age: {3}", this.Weight, this.Height, this.Name, this.age);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,12 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        SnakeGame game = new SnakeGame();
-        game.Run();
+        // SnakeGame game = new SnakeGame();
+        // game.Run();
+        Animal cat = new Cat("20kg", "1.5", "kitty");
+        cat.PrintInfo();
+        System.Console.WriteLine();
+        cat = new Kitten("10kg", "1", "kitty", 2);
+        cat.PrintInfo();
     }
 }
