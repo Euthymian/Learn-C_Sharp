d86fe7a8c2f335914210fe17bc5e861b20ccc855
GoldenGoat <thanhstryshard@gmail.com>
2024-04-24 21:42:35 -0700

    Measuring sorting time

--- Basic/Practice.cs
+++ Basic/Practice.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 static class Practice
 {
     public static void Linear()
@@ -233,11 +235,87 @@ static class Practice
             System.Console.WriteLine("Number " + i + " appears " + count[i] + " times");
         }
     }
-    public static int[] Test(int[] arr) {
-        arr[0] = 100;
-        return arr;
+    public static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
-    public static void Test(ref int a) {
-        a = 100;
+    public static void MergeSort(int[] arr)
+    {
+        int n = arr.Length;
+        if (n < 2)
+        {
+            return;
+        }
+        int mid = n / 2;
+        int[] left = new int[mid];
+        int[] right = new int[n - mid];
+        for (int i = 0; i < mid; i++)
+        {
+            left[i] = arr[i];
+        }
+        for (int i = mid; i < n; i++)
+        {
+            right[i - mid] = arr[i];
+        }
+        MergeSort(left);
+        MergeSort(right);
+        Merge(left, right, arr);
+    }
+    public static void Merge(int[] left, int[] right, int[] arr)
+    {
+        int nL = left.Length;
+        int nR = right.Length;
+        int i = 0, j = 0, k = 0;
+        while (i < nL && j < nR)
+        {
+            if (left[i] <= right[j])
+            {
+                arr[k] = left[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < nL)
+        {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < nR)
+        {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+    public static void MeasureSortTime() {
+        int[] arr = new int[100000];
+        Random rand = new Random();
+        for (int i = 0; i < 100000; i++)
+        {
+            arr[i] = rand.Next(1, 10000);
+        }
+        StopWatch sw = new StopWatch();
+        sw.Start();
+        BubbleSort(arr);
+        sw.Stop();
+        sw.GetElapsedTime();
     }
 }
