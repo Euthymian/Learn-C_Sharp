5ca4a9a931cf332ee66b76876b58164317fb72c6
GoldenGoat <thanhstryshard@gmail.com>
2024-04-26 20:59:44 -0700

    Inheritance

---
+++ Basic/Inheritance.cs
@@ -0,0 +1,100 @@
+class Human {
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Human(string name, int age) {
+        this.Name = name;
+        this.Age = age;
+    }
+    public virtual void PrintInfo() {
+        System.Console.WriteLine("Name: {0}" + '\n' + "Age: {1}", this.Name, this.Age);
+    }
+}
+
+class NormalPerson : Human {
+    public string Gender { get; set; }
+    public NormalPerson(string name, int age, string gender) : base(name, age) {
+        this.Gender = gender;
+    }
+    public override void PrintInfo() {
+        base.PrintInfo();
+        System.Console.WriteLine("Gender: {0}", this.Gender);
+    }
+}
+
+class SpecialPerson : NormalPerson {
+    public string Speciality { get; set; }
+    public SpecialPerson(string name, int age, string gender, string speciality) : base(name, age, gender) {
+        this.Speciality = speciality;
+    }
+    public override void PrintInfo() {
+        base.PrintInfo();
+        System.Console.WriteLine("Speciality: {0}", this.Speciality);
+    }
+}
+
+abstract class Vehicle {
+    public string Model { get; set; }
+    public Vehicle(string model) {
+        this.Model = model;
+    }
+    public virtual void PrintInfo() {
+        System.Console.WriteLine("Model: {0}", this.Model);
+    }
+
+    public abstract void Drive();
+}
+
+class Car : Vehicle {
+    public Car(string model) : base(model) {}
+    public override void Drive() {
+        System.Console.WriteLine("Driving a car");
+    }
+}
+
+public class BaseC
+{
+    public int x;
+    public void Invoke() { 
+        System.Console.WriteLine("BaseC.Invoke");
+    }
+    public int Print() {
+        System.Console.WriteLine("BaseC.Print");
+        return 0;
+    }
+    public virtual void Show() {
+        System.Console.WriteLine("BaseC.Show");
+    }
+}
+
+public class DerivedC : BaseC
+{
+    public new void Invoke() {
+        System.Console.WriteLine("DerivedC.Invoke");
+    }
+    public DerivedC() {
+        Print = 1;
+    }
+    public new int Print;
+    // Without "new" keyword, there will be a warning of 
+    // an existing member with the same name at the base class
+
+
+    public void InvokeBase() {
+        base.Invoke();
+    }
+
+    public sealed override void Show() {
+        System.Console.WriteLine("DerivedC.Show");
+    }
+
+    // The "sealed" keyword prevents the method from being overridden
+}
+
+public class DerivedD : DerivedC
+{
+    // public override void Show() {
+    //     System.Console.WriteLine("DerivedD.Show");
+    // }
+
+    // ---> This will cause an error because the method is sealed in the base class
+}
--- Program.cs
+++ Program.cs
@@ -5,16 +5,35 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        SnakeGame game = new SnakeGame();
-        game.Run();
+        // SnakeGame game = new SnakeGame();
+        // game.Run();
 
 
         // Animal cat = new Cat("20kg", "1.5", "kitty");
         // cat.PrintInfo();
         // System.Console.WriteLine();
+        // System.Console.WriteLine(cat.GetType());
+        // System.Console.WriteLine();
         // cat = new Kitten("10kg", "1", "kitty", 2);
         // cat.PrintInfo();
 
+
         // Practice.MeasureSortTime();
+
+        Human h1 = new SpecialPerson("John", 20, "Male", "Eye-Laser");
+        h1.PrintInfo();
+
+
+        Vehicle v1 = new Car("Car");
+        v1.Drive();
+
+        System.Console.WriteLine("\n\n");
+        DerivedC b = new DerivedC();
+        b.Invoke();
+        System.Console.WriteLine(b.Print);
+
+        System.Console.WriteLine();
+        BaseC baseC = new DerivedC();
+        baseC.Invoke();
     }
 }
